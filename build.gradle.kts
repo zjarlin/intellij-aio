a850d90c37ecc42ce863fc6bc09f2a28c85584a8
zjarlin <zjarlin@outlook.com>
2025-02-19 11:43:01 +0800

    动态注册意图

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,18 +57,8 @@ intellijPlatform {
 //            untilBuild = "262.*"
 //        }
 
-        fun String.ok(): String {
-            val run = File(projectDir, this).readText().run {
-                val apiKey = System.getenv("DASHSCOPE_API_KEY")
-                    ?: throw IllegalStateException("DASHSCOPE_API_KEY environment variable is not set")
-                val translatedContent = MarkdownTranslator.translateAndAppend(this, apiKey)
-                markdownToHTML(translatedContent)
-            }
-            return run
-        }
-
-        description = "README.md".ok()
-        changeNotes = "CHANGELOG.md".ok()
+        description = File(projectDir, "README.md").readText().let { markdownToHTML(it) }
+        changeNotes = File(projectDir, "CHANGELOG.md").readText().let { markdownToHTML(it) }
 
 
     }
@@ -114,5 +104,17 @@ tasks {
 
     publishPlugin {
         token.set(System.getenv("PUBLISH_TOKEN"))
+        doFirst {
+            val apiKey = System.getenv("DASHSCOPE_API_KEY") ?: throw IllegalStateException("DASHSCOPE_API_KEY environment variable is not set")
+            val readmeContent = File(projectDir, "README.md").readText()
+            val translatedReadme = MarkdownTranslator.translateAndAppend(readmeContent, apiKey)
+            val changelogContent = File(projectDir, "CHANGELOG.md").readText()
+            val translatedChangelog = MarkdownTranslator.translateAndAppend(changelogContent, apiKey)
+            
+            intellijPlatform.pluginConfiguration {
+                description = markdownToHTML(translatedReadme)
+                changeNotes = markdownToHTML(translatedChangelog)
+            }
+        }
     }
 }

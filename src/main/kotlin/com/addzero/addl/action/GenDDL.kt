a7582c0ba3bf53fee99b152322bcd8a63a0435db
zjarlin <zjarlin@outlook.com>
2025-03-06 13:30:36 +0800

    change

--- src/main/kotlin/com/addzero/addl/action/GenDDL.kt
+++ src/main/kotlin/com/addzero/addl/action/GenDDL.kt
@@ -27,7 +27,7 @@ class GenDDL : AnAction() {
         // 使用工具类检查是否为POJO或Jimmer实体
         val isValidTarget = PsiValidateUtil.isValidTarget(ktClass, psiClass)
 
-        e.presentation.isEnabled = project != null && isValidTarget.first
+        e.presentation.isEnabled =  isValidTarget.first
     }
 
 

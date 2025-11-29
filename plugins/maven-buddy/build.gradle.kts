fb6aace1986edc929a4c73984e2794483b57f780
zjarlin <zjarlin@outlook.com>
2025-11-29 19:49:59 +0800

    feat(maven-buddy): 配置插件元数据和依赖项
    - 在 build.gradle.kts 中添加插件配置块，设置插件 ID、名称和版本
    - 更新 build-logic 模块的依赖项，移除

--- plugins/maven-buddy/build.gradle.kts
+++ plugins/maven-buddy/build.gradle.kts
@@ -5,3 +5,12 @@ dependencies {
     implementation("site.addzero:tool-api-maven:2025.11.28")
     implementation("org.xerial:sqlite-jdbc:3.44.1.0")
 }
+
+val pluginName = project.name
+intellijPlatform {
+    pluginConfiguration {
+        id = "site.addzero.$pluginName"
+        name = pluginName
+        version = "2025.11.30"
+    }
+}

c6006b6970e224770a76825f75f5235e35e74be1
zjarlin <zjarlin@outlook.com>
2025-12-17 13:16:15 +0800

    feat(toolwindow): update auto-save functionality to use POJO class name as entity name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,12 +11,13 @@ implementationRemoteGit{
 }
 
 // >>> Gradle Buddy: On-Demand Modules (DO NOT EDIT THIS BLOCK) >>>
-// Generated at: 2025-12-17T11:38:50.948787
-// Loaded: 8, Excluded: 0, Total: 8
+// Generated at: 2025-12-17T12:54:12.144693
+// Loaded: 9, Excluded: 0, Total: 9
 include(":lib-git:metaprogramming-lsi:lsi-core")
 include(":lib-git:metaprogramming-lsi:lsi-ksp")
 include(":lib-git:metaprogramming-lsi:lsi-kt2")
 include(":lib-git:metaprogramming-lsi:lsi-psi")
+include(":lib-git:metaprogramming-lsi:lsi-reflection")
 include(":lib:ddlgenerator:tool-ddlgenerator")
 include(":lib:tool-swing")
 include(":plugins:autoddl")
